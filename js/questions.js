3538150c811ecc0a95ab872d65153ed8bed96531
paulineMaunes <maunes.pauline@gmail.com>
2016-10-26 02:20:24 -0700

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,62 @@
+ 
+var quiz = [{
+  "question": "When Samson killed 1000 Philistines at Lehi what weapon did he use? ",
+  "choices": ["jawbone of an ass", " Sword", " Power full arms"],
+  "correct": "javascriptwbone of an ass"
+}, {
+  "question": "How old was David when he was anointed King of Israel ?",
+  "choices": ["29 years", "32 years", "30 years"],
+  "correct": "30 years"
+}, {
+  "question": " How is the name “Emmanuel” interpreted?",
+  "choices": ["Glory of the God", "God is with us", "God is God"],
+  "correct": "God is with us"
+}, {
+  "question": "God tested Abraham by telling him to offer his only son as a burnt offering. Who was that son?",
+  "choices": ["Isaac", "Gabriel", " David"],
+  "correct": "Isaac"
+}, {
+  "question": "Who hid Joshua’s spies from Jericho’s king?",
+  "choices": ["Sarah", "Mary", "Rahab"],
+  "correct": "Rahab"  
+},{
+  "question": "How many years were the Israelites in bondage in Egypt?",
+  "choices": ["430", "431", "432"],
+  "correct": "430"
+},{
+  "question": "How old was Abraham when his son Isaac was born?",
+  "choices": ["99 years", "100 years", "101 years"],
+  "correct": "100 years"
+},{
+  "question": " Who took his rod and stretched out his hand upon the waters of Egypt to turn them into blood?",
+  "choices": ["John", "Abraham", "Aaron"],
+  "correct": "Aaron"
+},{
+  "question": "How tall was Goliath?",
+  "choices": ["8 cubits and a span", "6 cubits and a span", "9 cubits and a span"],
+  "correct": "6 cubits and a span"
+},{
+  "question": "Who were the parents of Cain and Abel?",
+  "choices": ["Adam and Eve", "Jacob and Raquel", "Abraham and Sarah"],
+  "correct": "Adam and Eve"
+},{
+  "question": "After the flood, what sign did God give that He would not destroy the earth with a flood again?",
+  "choices": ["Shooting Stars", "Rain", "Rainbow"],
+  "correct": "Rainbow"
+},{
+  "question": "Who did Boaz marry?",
+  "choices": ["Ruth", "Mary", "Sisa"],
+  "correct": "Ruth"
+},{
+  "question": "Abraham traveled with his nephew. What was his nephew’s name?",
+  "choices": ["Lottie", "Jonas", "Lot"],
+  "correct": "Lot"
+},{
+  "question": "What was the name of Joseph’s baby brother?",
+  "choices": ["Benjamin", "Lemuel", "Daniel"],
+  "correct": "Benjamin"
+},{
+  "question": "Noah sent out 2 different kinds of birds from the Ark. What were they?",
+  "choices": ["Dove and Duck", "Raven and Dove", "Owl and Raven"],
+  "correct": "Raven and Dove"
+}];
