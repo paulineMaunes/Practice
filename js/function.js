d6ea82e05cba605c707c376acfab69985dd542b4
paulineMaunes <maunes.pauline@gmail.com>
2016-10-31 07:56:16 -0700

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,5 +1,39 @@
     // var life = 3;
-   
+    var time_container = document.getElementById('span_timer');  
+      content = $("content"),
+      questionContainer = $("question"),
+      choicesContainer = $("choices"),
+      scoreContainer = $("score"),
+      submitBtn = $("submit"),
+      currentQuestion = 0,
+      quesnum = 0,
+      score = 0,
+      time = 4,
+      askingQuestion = true;
+
+
+
+    function timer() { 
+      var timerId = setInterval(function() {
+        //consolef.log(i);
+        time = time - 1;
+        if(time < 0) {
+          //alert("Times Up!");
+          checkAnswer();
+          time_container.style.color = "transparent";
+          
+        }else if(time < 4) {
+          time_container.style.color = "red"; 
+          time_container.innerHTML = time;
+        }else{
+          time_container.innerHTML = time;
+        }
+      }, 1000);
+    }
+    
+
+    timer();
+
 
     function $(id) { // shortcut for document.getElementById
       return document.getElementById(id);
@@ -10,10 +44,10 @@
         choicesHtml = "";
       // loop through choices, and create radio buttons
        for (var i = 0; i < choices.length; i++) {
-        choicesHtml += "<div id='style' class='btn-group  animated button fadeInUp waves-effect btn-block'><input type='radio' name='quiz" + currentQuestion +
+        choicesHtml += "<div class='col-md-4'><ul><li><input type='radio' class='animated button fadeInUp' name='quiz" + currentQuestion +
           "' id='choice" + (i + 1) +
           "' value='" + choices[i] + "'>" +
-          " <label for='choice" + (i + 1) + "'>" + choices[i] + "</label></div>";
+          " <label class='animated button fadeInUp' for='choice" + (i + 1) + "'>" + choices[i] + "</label></ul></li></div>";
 
         
       }
@@ -49,7 +83,7 @@
 
             var labelStyle = document.getElementsByTagName("label")[correctIndex].style;
             labelStyle.fontWeight = "bold";
-            labelStyle.color = "#3b5998";
+            labelStyle.color = "green";
 
 
             if (userpick == quiz[currentQuestion].correct) {
@@ -71,7 +105,7 @@
             if (currentQuestion < quiz.length - 1) {
               currentQuestion++;
               askQuestion();
-              time = 100;}
+              time = 4;}
 
             else {
               showFinalResults();}
@@ -82,14 +116,18 @@
     function showFinalResults() {
         var stime = document.getElementById('timming'),
             cat = document.getElementById('examType');
-        content.innerHTML = "<br><h2><br>Thank you!</h2>" +
+        content.innerHTML = "<br><h2>You've completed the quiz!</h2>" +
         "<h2>Below are your results:</h2>" +
         "<h2>" + score + " out of " + quiz.length + " questions, " +
         Math.round(score / quiz.length * 100) + "%</h2>";
         time = 0;
-        stime.style.color = "transparent";
-        
-
+        stime.style.visibility = "hidden";
+        cat.style.color = "transparent";
+        window.alert = function(){
+          window.alert = function(text){
+            "andadshkajhd";
+          }
+        }
     }
 
     window.addEventListener("load", askQuestion, false);
